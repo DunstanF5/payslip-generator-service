17fba8d010fca871cfe074c83f633209a310cd86
DunstanF5 <dunstan.darshana@fusion5.co.nz>
2026-08-26 12:34:56 +1200

    code review workflow testing

--- src/main/java/com/learning/payslip/config/SecurityConfig.java
+++ src/main/java/com/learning/payslip/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(new ApiKeyAuthFilter(apiKey), UsernamePasswordAuthenticationFilter.class);
-
+        System.out.println("Payslip downloaded for jobId: ");
+        //todo
         return http.build();
     }
 
